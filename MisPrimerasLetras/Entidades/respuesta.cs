e62c55a00d6c6d312b8c880871c50cf5d3a6c1d8
David <davidescobar007@hotmail.com>
2020-03-02 22:17:33 -0500

    conexion con base de datos

---
+++ MisPrimerasLetras/Entidades/respuesta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace Entidades
+{
+    public class Respuesta <T>
+    {
+        
+        public int ResponseCode { get; set; }          
+        
+        public string ResponseMessage { get; set; }         
+                 
+        public string IdTransactionCode { get; set; }       
+
+        public int RowsAffected { get; set; }
+
+        public Collection<T> ResultData { get; set; }
+
+    }
+}
